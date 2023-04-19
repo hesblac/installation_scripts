788a06e2a3f3d1af33b9145c7a736e2969c5745b
Stephen Olaiya <54467284+hesblac@users.noreply.github.com>
2023-04-19 05:38:56 +0100

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,51 +1,27 @@
-#!/bin/bash
-
-
-sudo apt-get update -y
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg
-
-
-sudo mkdir -m 0755 -p /etc/apt/keyrings
+    
+    
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt-get update
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo chmod 666 /var/run/docker.sock
-
-echo "Apply updates"
-sudo apt update -y 
-sudo apt upgrade -y
-
-echo " Download Minikube Binary"
-wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo cp minikube-linux-amd64 /usr/local/bin/minikube
-sudo chmod +x /usr/local/bin/minikube
-minikube version
+sudo docker run hello-world
 
 
-echo "Install Kubectl utility"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-kubectl version -o yaml
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-sudo usermod -aG docker $USER && newgrp docker
-echo "Start the minikube"
-minikube start 
-minikube status
-
-
+minikube start --container-runtime=docker

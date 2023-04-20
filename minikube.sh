c30646097f6dc65b12bf7b1dd4e89a45efa420cd
Stephen Olaiya <54467284+hesblac@users.noreply.github.com>
2023-04-20 06:15:16 +0100

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -27,4 +27,11 @@ sudo docker run hello-world
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
+echo "Install Kubectl utility"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version -o yaml
+
+
 minikube start --container-runtime=docker

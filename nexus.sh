1fe17ee8fe1b0a5e4252ad222ad45f4d971b367b
Stephen Olaiya <54467284+hesblac@users.noreply.github.com>
2023-04-23 11:24:09 +0100

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -8,8 +8,8 @@ echo "Install Nexus"
 useradd -M -d /opt/nexus -s /bin/bash -r nexus
 echo "nexus ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nexus
 mkdir /opt/nexus
-wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
-tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
+wget 	https://download.sonatype.com/nexus/3/nexus-3.52.0-01-unix.tar.gz 
+tar xzf nexus-3.52.0-01-unix.tar.gz -C /opt/nexus --strip-components=1
 chown -R nexus:nexus /opt/nexus
 
 nano /opt/nexus/bin/nexus.vmoptions

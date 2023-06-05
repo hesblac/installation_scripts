879a2f96d838aa94f4d1fabe37c7cf8f1f0f578c
Stephen Olaiya <54467284+hesblac@users.noreply.github.com>
2023-06-05 08:33:12 +0100

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -14,9 +14,13 @@ wget 	https://download.sonatype.com/nexus/3/nexus-3.52.0-01-unix.tar.gz
 tar xzf nexus-3.52.0-01-unix.tar.gz -C /opt/nexus --strip-components=1
 chown -R nexus:nexus /opt/nexus
 
-sudo vim /opt/nexus/bin/nexus.vmoptions
-sudo vim /opt/nexus/bin/nexus.rc
-sudo -u nexus /opt/nexus/bin/nexus start
+
+# sudo sed -i 's/\.\./\./g' /opt/nexus/bin/nexus.vmoptions
+# sudo sh -c 'echo "run_as_user=\"nexus\"" > /opt/nexus/bin/nexus.rc'
+
+# sudo vim /opt/nexus/bin/nexus.vmoptions
+# sudo vim /opt/nexus/bin/nexus.rc
+# sudo -u nexus /opt/nexus/bin/nexus start
 
 
 
